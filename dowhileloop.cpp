7ae37b2683cfaa10c321f056fc54a0dbbd353862
shivamsharma809 <138153296+shivamsharma809@users.noreply.github.com>
2023-07-19 10:44:12 +0530

    Add files via upload

---
+++ dowhileloop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int password;
+    cout<<"enter the password"<<endl;
+
+    do
+    {
+        cin>>password;
+    
+
+    } while (password<999999);
+    {
+        cout<<"The password is generated";
+    }
+    
+}
